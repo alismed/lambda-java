7d69670207eb0a0b5517d6565ae2f9a5bdf0dcec
Alison Souza <alisonmsouza@yahoo.com.br>
2025-03-09 14:15:28 -0300

    review

--- infra/main.tf
+++ infra/main.tf
@@ -1,31 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.89.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "2.7.0"
-    }
-  }
-}
-
-provider "aws" {
-  region  = var.region
-  # If terraform.workspace is "default" (GitHub Actions), profile is set to null (not used)
-  dynamic "default_tags" {
-    for_each = terraform.workspace == "default" ? [] : [1]
-    content {
-      tags = {
-        Environment = "local"
-      }
-    }
-  }
-  # If terraform.workspace is not "default" (local), uses the profile from var.profile (lstk)
-  profile = terraform.workspace == "default" ? null : var.profile
-}
-
 resource "aws_iam_role" "lambda_role" {
   name               = "GetTime_Lambda_Role"
   assume_role_policy = file("trust/GetTime_Lambda_role.json")
